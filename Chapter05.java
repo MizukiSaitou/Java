4fbeea5cb4ea14775295f502a847aa2c7ec2b798
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-08-22 00:30:08 +0900

    型の基本

---
+++ Chapter05.java
@@ -0,0 +1,28 @@
+public class Chapter05{
+
+    public static void main(String[] args){
+      long num1 = 12345678910L;
+      double  num2 = 109876543.21;
+      boolean type = true;
+      System.out.println(num1);
+      System.out.println(num2);
+      System.out.println(type);
+
+      Float num3 = 14.142f;
+      Integer num4 = 19941201;
+      Character c = 'あ';
+      System.out.println(num3);
+      System.out.println(num4);
+      System.out.println(c);
+
+      int[] array1 = {1, 2, 3, 4, 5};
+      System.out.println(array1[3]);
+    }
+}
+
+
+
+//   .プリミティブ型の「long」「double」「boolean」の変数に値を代入して出力
+// 　・ラッパークラスの「Float」「Integer」「Character」の変数に値を代入して出力
+// 　・要素数が5の「int」配列に値を代入して、インデックス番号が3の値を出力
+
