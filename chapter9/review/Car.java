7fa14ed1edcd1b0835fd997a06b9d498f1cabcfb
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-09-25 23:58:24 +0900

    Add review

--- chapter9/review/Car.java
+++ chapter9/review/Car.java
@@ -7,11 +7,22 @@ public class Car {
     private double fuelAmount;
 
     // コンストラクタを作成
-    
+    public Car(double fuelCost, double fuelAmount){
+      this.fuelCost = fuelCost;
+      this.fuelAmount = fuelAmount;
+    }
 
     // moveメソッド
-    // ・"xx km 走ります"を出力
-    // ・残量を計算
+    public void move(int d){
+      // ・"xx km 走ります"を出力
+      System.out.println(d + "km走ります");
+      // ・残量を計算
+      this.fuelAmount -= (d / fuelCost);
+    }
+    
 
     // fuelAmountを取得するメソッドを作成
+    public double getFuelAmount(){
+      return this.fuelAmount;
+    }
 }
